02febd896f7c741ec98da61a1d5d82c6b6df2a3b
Mittenover <lucien.perdrix@gmail.com>
2023-05-09 11:37:42 +0200

    Conclusion

--- main.R
+++ main.R
@@ -33,7 +33,8 @@ summary(df)  #Toutes les variables sauf la dernière sont numériques
 str(df)
 
 
-####Analyse bivariée (à voir)
+####Analyse bivariée 
+corrplot(cor(df[,-192]))
 
 
 ####Proportion de chacun des genres
@@ -110,7 +111,7 @@ ModT<-model(formuleT,data=df[train==TRUE,])
 ####Modele1####
 ###############
 
-####Récupérations des variables significatives
+####Récupération des variables significatives
 library(stats)
 ind_variables_sign=which(summary(ModT)$coefficients[,4]<=0.05)
 names(df)[ind_variables_sign]
@@ -126,11 +127,11 @@ Mod1=model(formule1,data=df[train==TRUE,])
 ####Modele2####
 ###############
 
-####Récupérations des variables significatives
+####Récupération des variables significatives
 ind_variables_sign=which(summary(ModT)$coefficients[,4]<=0.2)
 names(df)[ind_variables_sign]
 
-####Mis à jour du modèle en supprimant les variables significatives
+####Mis à jour du modèle en supprimant les variables non-significatives
 formule2<-GENRE~PAR_TC+PAR_SC_V+PAR_ASE2+PAR_ASE3+PAR_ASE4+PAR_ASE5+PAR_ASE6+PAR_ASE8+PAR_ASE13+PAR_ASE16+PAR_ASE17+PAR_ASE18+PAR_ASE19+PAR_ASE21+PAR_ASE22+PAR_ASE23+PAR_ASE24+
   PAR_ASE25+PAR_ASE28+PAR_ASE29+PAR_ASE31+PAR_ASE32+PAR_ASEV2+PAR_ASEV7+PAR_ASEV8+PAR_ASEV9+PAR_ASEV10+PAR_ASEV11+PAR_ASEV14+PAR_ASEV19+PAR_ASEV21+PAR_ASEV22+PAR_ASEV24+
   PAR_ASEV25+PAR_ASEV26+PAR_ASEV29+PAR_ASEV30+PAR_ASEV31+PAR_ASC+PAR_ASC_V+PAR_ASS+PAR_SFM1+PAR_SFM3+PAR_SFM4+PAR_SFM6+PAR_SFM7+PAR_SFM8+PAR_SFM9+PAR_SFM10+PAR_SFM11+PAR_SFM12+
@@ -142,7 +143,8 @@ Mod2=model(formule2,data=df[train==TRUE,])
 ####ModAIC####
 ###############
 library(MASS)
-#ModAIC=stepAIC(ModT,scope=list(upper=GENRE~.,lower=GENRE~1))
+ModAIC=stepAIC(glm(GENRE~.,family=binomial(link="logit"),data=df[train==TRUE,]),direction="both")
+#Ne marche pas
 
 ################
 ### Q4
@@ -191,8 +193,12 @@ pred=predict(Mod0,newdata=df[train==FALSE,])
 ROC_0_test=roc(df$GENRE[train==FALSE],pred)
 lines(ROC_0_test, xlab="", col=4,main="Superposition courbes ROC")
 
-legend(0.8,0.3,legend=c(paste("ModT :",toString(auc(ROC_T_test))),paste("Mod1 :",toString(auc(ROC_1_test))),paste("Mod2 :",toString(auc(ROC_2_test))),paste("Mod0 :",toString(auc(ROC_1_test)))), col=c(1:4),lty=1)
+pred=predict(ModAIC,newdata=df[train==FALSE,])
+ROC_AIC_test=roc(df$GENRE[train==FALSE],pred)
+lines(ROC_AIC_test, xlab="", col=4,main="Superposition courbes ROC")
 
+legend(0.8,0.3,legend=c(paste("ModT :",toString(auc(ROC_T_test))),paste("Mod1 :",toString(auc(ROC_1_test))),paste("Mod2 :",toString(auc(ROC_2_test))),paste("Mod0 :",toString(auc(ROC_1_test))),paste("ModAIC :",toString(auc(ROC_AIC_test)))), col=c(1:5),lty=1)
+png("Superpositionmodele.png")
 
 ################
 ### Q4
@@ -225,9 +231,7 @@ error_classif(df[train==TRUE,],Mod2,0.5) #0.1060766
 error_classif(df[train==FALSE,],Mod2,0.5) #0.1306778
 
 ##ModAIC
-#Apprentissage
-
-#Test
+error_classif(df[train==FALSE,],ModAIC,0.5) #0.1123569
 
 
 
@@ -300,6 +304,7 @@ mean((glm.pred-y.test)^2) #0.0979418
 ################
 ### Q1
 ################
+
 library(MASS)
 library(class)
 #Mise en forme données
@@ -338,7 +343,8 @@ for(i in 1:nK)
   print(k)
 }
 stopCluster(cl)
-#On a pas pu aller plus loin que 500 itérations
+
+#On n'a pas pu aller plus loin que 500 itérations
 ErrTrain=ErrTrain[1:498]
 ErrTest=ErrTest[1:498]
 K=K[1:498]
@@ -357,4 +363,18 @@ which.min(ErrTest)  #k=1
 #On a donc un cas d'underfitting (risque de biais). 
 #On voit que le modèle est peu performant avec une erreur de test de 0.36
 
+##########################################################################################################################################
+##########################################################################################################################################
+##########################################################Conclusion######################################################################
+##########################################################################################################################################
+##########################################################################################################################################
 
+#D'après les résultats précédents le modèle le plus performant en généralisation est le modèle de régression ridge avec une erreur de classification 
+#de 0.09652236.
+
+test=read.table("Music_test.txt",dec='.',header=TRUE,sep=';')
+x=as.matrix(test)
+pred=predict(Mod,s=lambda,newx=x)
+pred=ifelse(pred>0.5,1,0)
+pred=ifelse(pred==1,"Jazz","Classical")
+write.table(pred,"LIMES-PERDRIX_test.txt")
